309e40025412f544a4d81ed088055f4f734b6637
sarayubolla <sarayu.bolla@gmail.com>
2020-07-21 20:02:11 +0530

    commitmsg

---
+++ src/main/java/task8/task8/App.java
@@ -0,0 +1,22 @@
+package task8.task8;
+
+public class App {
+	
+
+	public String valid_string(String str) {
+		String m="";
+		for(int i=0;i<2;i++) {
+			if(str.charAt(i)!='A') {
+				m=m+str.charAt(i);
+				
+			
+			}
+		}
+		m=m+str.substring(2);
+		return m;
+		
+	}
+	
+	
+
+}
